28769a54052414805cfed05ad67024f69c23a997
RubberBear961 <rubberbear@op.pl>
2025-09-07 15:13:26 +0200

    removed comments

--- src/main/java/net/bieluuu/hardproof/HardProofClient.java
+++ src/main/java/net/bieluuu/hardproof/HardProofClient.java
@@ -1,7 +1,4 @@
 package net.bieluuu.hardproof;
-
-//test comment to check whether git works
-
 import net.fabricmc.api.ClientModInitializer;
 
 public class HardProofClient implements ClientModInitializer {
