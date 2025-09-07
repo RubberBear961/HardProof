8304f1c5c0f3bf09c67971fd5abf48af5e09041c
RubberBear961 <rubberbear@op.pl>
2025-09-07 15:11:36 +0200

    Initial commit

---
+++ src/main/java/net/bieluuu/hardproof/HardProof.java
@@ -0,0 +1,16 @@
+package net.bieluuu.hardproof;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HardProof implements ModInitializer {
+	public static final String MOD_ID = "hardproof";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/bieluuu/hardproof/HardProofClient.java
@@ -0,0 +1,12 @@
+package net.bieluuu.hardproof;
+
+//test comment to check whether git works
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HardProofClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
